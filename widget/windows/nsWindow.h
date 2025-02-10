037717a32ac4333de52f2346ce955f858fc7eb46
K4sum1 <k4sum1@eclipse.cx>
2025-02-10 02:23:32 -0600

    Mostly fix XP theme
    The css @media thing seems to be broken (at least on 2003 with OCAPI), idk why. I think I did it right but I'm also r3tard.

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -634,6 +634,7 @@ class nsWindow final : public nsBaseWidget {
   static bool IsTopLevelMouseExit(HWND aWnd);
   LayoutDeviceIntRegion GetRegionToPaint(bool aForceFullRepaint, PAINTSTRUCT ps,
                                          HDC aDC);
+  static void ActivateOtherWindowHelper(HWND aWnd);
   nsIWidgetListener* GetPaintListener();
 
   void AddWindowOverlayWebRenderCommands(
@@ -690,6 +691,7 @@ class nsWindow final : public nsBaseWidget {
   static bool sJustGotDeactivate;
   static bool sJustGotActivate;
   static bool sIsInMouseCapture;
+  //static int sTrimOnMinimize;
   static bool sIsRestoringSession;
 
   // Message postponement hack. See the definition-site of
