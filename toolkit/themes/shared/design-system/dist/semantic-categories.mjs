1576c4a8cb3182297b7fd2a355cdf2169e4fe4a9
K4sum1 <k4sum1@eclipse.cx>
2025-06-07 02:56:37 -0500

    Disable shitty new UI
    Fuck rounded corners
    
    Revert the over rounding of elements from this.
    https://bugzilla.mozilla.org/show_bug.cgi?id=1965867
    
    Hopefully temporary color fixes
    
    Temporary because it's a Mozilla issue, also hopefully because it's a Mozilla issue.
    
    Disable more shitty new UI

--- toolkit/themes/shared/design-system/dist/semantic-categories.mjs
+++ toolkit/themes/shared/design-system/dist/semantic-categories.mjs
@@ -918,7 +918,7 @@ export const tokensTable = {
     { value: "var(--border-radius-medium)", name: "--button-border-radius" },
     { value: "var(--border-radius-large)", name: "--card-border-radius" },
     {
-      value: "var(--border-radius-medium)",
+      value: "var(--border-radius-small)",
       name: "--input-text-border-radius",
     },
     {
@@ -3415,7 +3415,7 @@ export const variableLookupTable = {
   "input-text-border-color": "var(--border-color)",
   "input-text-border-color-disabled":
     "var(--border-color-interactive-disabled)",
-  "input-text-border-radius": "var(--border-radius-medium)",
+  "input-text-border-radius": "var(--border-radius-small)",
   "input-text-color": "FieldText",
   "input-text-color-disabled": {
     default: "var(--input-text-color)",
--- toolkit/themes/shared/design-system/dist/tokens-table.mjs
+++ toolkit/themes/shared/design-system/dist/tokens-table.mjs
@@ -892,10 +892,10 @@ export const tokensTable = {
     { value: "4px", name: "--border-radius-small" },
     { value: "8px", name: "--border-radius-medium" },
     { value: "16px", name: "--border-radius-large" },
-    { value: "var(--border-radius-medium)", name: "--button-border-radius" },
+    { value: "var(--border-radius-small)", name: "--button-border-radius" },
     { value: "var(--border-radius-large)", name: "--card-border-radius" },
     {
-      value: "var(--border-radius-medium)",
+      value: "var(--border-radius-small)",
       name: "--input-text-border-radius",
     },
     {
@@ -3216,7 +3216,7 @@ export const variableLookupTable = {
   },
   "button-border-color-toolbar-selected":
     "var(--button-border-color-ghost-active)",
-  "button-border-radius": "var(--border-radius-medium)",
+  "button-border-radius": "var(--border-radius-small)",
   "button-font-size": "var(--font-size-root)",
   "button-font-size-small": "var(--font-size-small)",
   "button-font-weight": "var(--font-weight-semibold)",
@@ -3417,7 +3417,7 @@ export const variableLookupTable = {
   "input-text-border-color": "var(--border-color)",
   "input-text-border-color-disabled":
     "var(--border-color-interactive-disabled)",
-  "input-text-border-radius": "var(--border-radius-medium)",
+  "input-text-border-radius": "var(--border-radius-small)",
   "input-text-color": "FieldText",
   "input-text-color-disabled": {
     default: "var(--input-text-color)",
