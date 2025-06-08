26358f142ed8a17ef98ba7932863b05343a4eb2e
K4sum1 <k4sum1@eclipse.cx>
2025-06-08 07:58:36 -0500

    Revert incorrect UAO optimization that broke SSUAO

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -513,7 +513,6 @@ class nsHttpChannel final : public HttpBaseChannel,
   [[nodiscard]] nsresult OpenCacheInputStream(nsICacheEntry* cacheEntry,
                                               bool startBuffering);
 
-  void SetLoadGroupUserAgentOverride();
   void SetOriginHeader();
   void SetDoNotTrack();
   void SetGlobalPrivacyControl();
