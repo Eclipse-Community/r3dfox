c62fb236c8e65cd149723811cd35493fdf8e1414
K4sum1 <k4sum1@eclipse.cx>
2025-06-15 22:43:10 -0500

    Major Librewolf changes stock
    Renamed lw directory to r3dfox

--- browser/components/urlbar/UrlbarProviderInterventions.sys.mjs
+++ browser/components/urlbar/UrlbarProviderInterventions.sys.mjs
@@ -446,7 +446,7 @@ export class UrlbarProviderInterventions extends UrlbarProvider {
         ]),
       });
       for (let [id, phrases] of Object.entries(DOCUMENTS)) {
-        queryScorer.addDocument({ id, phrases });
+//        queryScorer.addDocument({ id, phrases });
       }
       return queryScorer;
     },
