65bdbe30a9cf8b3038891f65e8ed3821d44dd2f4
K4sum1 <k4sum1@eclipse.cx>
2025-08-26 09:09:53 -0500

    Bug 1909311 - Remove nsIFormControlFrame and nsITextControlFrame. r=masayuki,layout-reviewers,dholbert    2024-07-24 00:16:18    d25d53b6fc1d8dde2251d547b9a5f41f7f2444a8

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -445,7 +445,6 @@
 #include "nsStringIterator.h"
 #include "nsStyleSheetService.h"
 #include "nsStyleStruct.h"
-#include "nsTextControlFrame.h"
 #include "nsSubDocumentFrame.h"
 #include "nsTextNode.h"
 #include "nsURLHelper.h"
@@ -14441,8 +14440,9 @@ already_AddRefed<nsDOMCaretPosition> Document::CaretPositionFromPoint(
     nsINode* nonChrome =
         node->AsContent()->FindFirstNonChromeOnlyAccessContent();
     HTMLTextAreaElement* textArea = HTMLTextAreaElement::FromNode(nonChrome);
-    nsTextControlFrame* textFrame =
+    nsITextControlFrame* textFrame =
         do_QueryFrame(nonChrome->AsContent()->GetPrimaryFrame());
+
     if (!textFrame) {
       return nullptr;
     }
