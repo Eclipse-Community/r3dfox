82c9f82b944660489d3d5f80e23dc70db973b5e6
K4sum1 <k4sum1@eclipse.cx>
2025-08-26 06:46:03 -0500

    REMOVE FOR 150 Jumpist builder changes
    Remove legacy jumplist builder
    
    Revert "Bug 1880082 - Let the off-main-thread WindowsJumpList backend ride the trains. r=florian 2024-02-14 00:07:57 b5baaefc592f6f00dbcb0b1427ef48205fe23295"
    
    This reverts commit a91d3a9166a1b1805311c1a61079ab4e287b151d.
    
    Revert "Bug 1867340 - Enable the new Windows Jump List backend on Nightly only. r=mhowell 2023-11-30 03:58:58 55021f5329b63b068d19daf14059c0858d4a25f8"
    
    This reverts commit 9ea5def65fe531d9b6ae681e7297da250edaa5fd.

--- widget/windows/JumpListBuilder.cpp
+++ widget/windows/JumpListBuilder.cpp
@@ -53,6 +53,7 @@ class NativeJumpListBackend : public JumpListBackend {
 
     mscom::EnsureMTA([&]() {
       RefPtr<ICustomDestinationList> destList;
+      CoInitialize(nullptr);
       HRESULT hr = ::CoCreateInstance(
           CLSID_DestinationList, nullptr, CLSCTX_INPROC_SERVER,
           IID_ICustomDestinationList, getter_AddRefs(destList));
@@ -608,6 +609,7 @@ void JumpListBuilder::DoPopulateJumpList(
 
   if (aTaskDescriptions.Length()) {
     RefPtr<IObjectCollection> taskCollection;
+    CoInitialize(nullptr);
     hr = CoCreateInstance(CLSID_EnumerableObjectCollection, nullptr,
                           CLSCTX_INPROC_SERVER, IID_IObjectCollection,
                           getter_AddRefs(taskCollection));
@@ -648,6 +650,7 @@ void JumpListBuilder::DoPopulateJumpList(
   if (aCustomDescriptions.Length()) {
     // Then build the custom list
     RefPtr<IObjectCollection> customCollection;
+    CoInitialize(nullptr);
     hr = CoCreateInstance(CLSID_EnumerableObjectCollection, nullptr,
                           CLSCTX_INPROC_SERVER, IID_IObjectCollection,
                           getter_AddRefs(customCollection));
@@ -830,6 +833,7 @@ nsresult JumpListBuilder::GetShellLinkFromDescription(
   // http://msdn.microsoft.com/en-us/library/bb774950(VS.85).aspx
 
   // Create a IShellLink
+  CoInitialize(nullptr);
   hr = CoCreateInstance(CLSID_ShellLink, nullptr, CLSCTX_INPROC_SERVER,
                         IID_IShellLinkW, (LPVOID*)&psl);
   if (FAILED(hr)) {
