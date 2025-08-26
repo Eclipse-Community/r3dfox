16adf1d18f015e016ccae07760fcc89b98fab692
K4sum1 <k4sum1@eclipse.cx>
2025-08-26 09:46:52 -0500

    Bug 1866022 - Teach GTK to render non-native tree columns and toolbars. r=dao,stransky,desktop-theme-reviewers    2023-12-05 02:57:35    1b1835b4aa260f548605f740f61e670edbce5279

--- widget/windows/nsLookAndFeel.cpp
+++ widget/windows/nsLookAndFeel.cpp
@@ -236,22 +236,18 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
     switch (aID) {
       case ColorID::MozButtonhoverborder:
       case ColorID::MozButtonhoverface:
-      case ColorID::MozColheaderhover:
       case ColorID::MozButtonactivetext:
-      case ColorID::MozColheaderactivetext:
         aColor = GetColorForSysColorIndex(COLOR_HIGHLIGHT);
         return NS_OK;
       case ColorID::MozButtonhovertext:
       case ColorID::MozColheaderhovertext:
       case ColorID::MozButtonactiveface:
-      case ColorID::MozColheaderactive:
         aColor = GetColorForSysColorIndex(COLOR_HIGHLIGHTTEXT);
         return NS_OK;
       case ColorID::MozButtondisabledborder:
         aColor = GetColorForSysColorIndex(COLOR_GRAYTEXT);
         return NS_OK;
       case ColorID::Buttonface:
-      case ColorID::MozColheader:
       case ColorID::MozButtondisabledface:
       case ColorID::MozDisabledfield:
       case ColorID::Field:
@@ -313,9 +309,6 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
     case ColorID::MozButtonhoverface:
     case ColorID::MozButtonactiveface:
     case ColorID::MozButtondisabledface:
-    case ColorID::MozColheader:
-    case ColorID::MozColheaderhover:
-    case ColorID::MozColheaderactive:
       idx = COLOR_BTNFACE;
       break;
     case ColorID::Buttonhighlight:
@@ -428,7 +421,6 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
     case ColorID::MozDialogtext:
     case ColorID::MozColheadertext:
     case ColorID::MozColheaderhovertext:
-    case ColorID::MozColheaderactivetext:
       idx = COLOR_WINDOWTEXT;
       break;
     case ColorID::Linktext:
