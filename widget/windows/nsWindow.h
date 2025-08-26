25d05d4b1978a0e430f40966322a3071ba9d449a
K4sum1 <k4sum1@eclipse.cx>
2025-08-26 09:22:22 -0500

    Bug 1946365 - Track already-clear region of transparent windows. r=win-reviewers,rkraesig    2025-02-19 01:56:00    250bf55503ba25535d10a72c95aaca10c57d14a7

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -231,7 +231,6 @@ class nsWindow final : public nsBaseWidget,
   [[nodiscard]] nsresult GetRestoredBounds(LayoutDeviceIntRect& aRect) override;
   LayoutDeviceIntRect GetClientBounds() override;
   LayoutDeviceIntPoint GetClientOffset() override;
-  LayoutDeviceIntSize GetSize() const;
   void SetCursor(const Cursor&) override;
   bool PrepareForFullscreenTransition(nsISupports** aData) override;
   void PerformFullscreenTransition(FullscreenTransitionStage aStage,
@@ -550,7 +549,7 @@ class nsWindow final : public nsBaseWidget,
   bool UpdateNonClientMargins(bool aReflowWindow = true);
   void UpdateDarkModeToolbar();
   void ResetLayout();
-  LayoutDeviceIntRegion ComputeNonClientRegion();
+  nsAutoRegion ComputeNonClientHRGN();
   HWND GetOwnerWnd() const { return ::GetWindow(mWnd, GW_OWNER); }
   bool IsOwnerForegroundWindow() const {
     HWND owner = GetOwnerWnd();
@@ -642,10 +641,6 @@ class nsWindow final : public nsBaseWidget,
   LayoutDeviceIntRegion GetOpaqueRegionForTesting() const override {
     return mOpaqueRegion;
   }
-  // Gets the translucent region, relative to the whole window, including the
-  // NC area.
-  LayoutDeviceIntRegion GetTranslucentRegion();
-  void MaybeInvalidateTranslucentRegion();
 
   void SetColorScheme(const mozilla::Maybe<mozilla::ColorScheme>&) override;
   void SetMicaBackdrop(bool) override;
@@ -845,9 +840,6 @@ class nsWindow final : public nsBaseWidget,
 
   // Graphics
   LayoutDeviceIntRect mLastPaintBounds;
-  // The region of the window we know is cleared to transparent already
-  // (relative to the whole window).
-  LayoutDeviceIntRegion mClearedRegion;
 
   ResizeState mResizeState = NOT_RESIZING;
 
