7586a9540c3c25e2f36920078706671a6f783fc2
K4sum1 <k4sum1@eclipse.cx>
2025-10-17 14:29:24 -0500

    Pref changes

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3175,7 +3175,7 @@ pref("extensions.webextensions.identity.redirectDomain", "extensions.allizom.org
 pref("extensions.webextensions.restrictedDomains", "accounts-static.cdn.mozilla.net,accounts.firefox.com,addons.cdn.mozilla.net,addons.mozilla.org,api.accounts.firefox.com,content.cdn.mozilla.net,discovery.addons.mozilla.org,oauth.accounts.firefox.com,profile.accounts.firefox.com,support.mozilla.org,sync.services.mozilla.com");
 
 // Extensions are prevented from accessing Quarantined Domains by default.
-pref("extensions.quarantinedDomains.enabled", true);
+pref("extensions.quarantinedDomains.enabled", false);
 pref("extensions.quarantinedDomains.list", "");
 
 // Include origin permissions in the install prompt for MV3 extensions.
