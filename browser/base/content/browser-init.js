de53ffc0dfb1942731e71243bef6c9a217598f62
the_r3dacted <k4sum1@eclipse.cx>
2025-10-19 00:31:01 -0500

    Update browser-init.js

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -96,7 +96,6 @@ var gBrowserInit = {
       if (nativeMenubar) {
         toolbarMenubar.removeAttribute("autohide");
       } else {
-        toolbarMenubar.removeAttribute("autohide");
         document.l10n.setAttributes(
           toolbarMenubar,
           "toolbar-context-menu-menu-bar-cmd"
