11531ed7df8678df00bfbd4a984111f83533f139
the_r3dacted <k4sum1@eclipse.cx>
2025-10-19 18:35:04 -0500

    Reduced telemetry

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -395,8 +395,6 @@ function init_all() {
     this.removeAttribute("keyboard-navigation");
   });
 
-  maybeDisplayPoliciesNotice();
-
   window.addEventListener("hashchange", onHashChange);
 
   document.getElementById("focusSearch1").addEventListener("command", () => {
