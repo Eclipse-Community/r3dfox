1ce5745c7a4ef193aef7149eddd281b09ac92516
the_r3dacted <k4sum1@eclipse.cx>
2025-11-05 13:01:13 -0600

    r3dfox theme and settings
    Disable urlbar seperators by default
    
    Firefox view is now taken care of by Librewolf patch.

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -12,6 +12,7 @@
 /* import-globals-from sync.js */
 /* import-globals-from experimental.js */
 /* import-globals-from moreFromMozilla.js */
+/* import-globals-from r3dfox.js */
 /* import-globals-from findInPage.js */
 /* import-globals-from /browser/base/content/utilityOverlay.js */
 /* import-globals-from /toolkit/content/preferencesBindings.js */
@@ -398,6 +399,7 @@ function init_all() {
   register_module("panePrivacy", gPrivacyPane);
   register_module("paneLibrewolf", gLibrewolfPane);
   register_module("paneContainers", gContainersPane);
+  register_module("paneR3dfox", gR3dfoxPane);
 
   if (ExperimentAPI.labsEnabled) {
     // Set hidden based on previous load's hidden value or if Nimbus is
