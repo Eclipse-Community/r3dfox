d70e7ca367685eb405bc0f509932210748f3a856
the_r3dacted <k4sum1@eclipse.cx>
2025-11-05 13:01:13 -0600

    r3dfox theme and settings
    Disable urlbar seperators by default
    
    Firefox view is now taken care of by Librewolf patch.
    
    Update r3dfox.css
    
    Make tab counter background size change with density sizes
    
    So it's no longer stuck small on all three sizes and doesn't look scuffed anymore
    
    Mozilla renamed css defines in r3dfox.css
    
    r3dfox.css add missing color-accent-primary-selected
    
    r3dfox.css button-primary-* no longe exists
    
    r3dfox.css add urlbar-box-background-color-* to no longer make url items blue
    
    Revert "test alt purple"
    
    This reverts commit d759b4286c98aeb18dc2a8016f76be33427b8ed6.
    
    test alt purple
    
    Replace red private gradient with a much darker and less intense gradient
    
    vbe

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -10,6 +10,7 @@
 /* import-globals-from privacy.js */
 /* import-globals-from sync.js */
 /* import-globals-from moreFromMozilla.js */
+/* import-globals-from r3dfox.js */
 /* import-globals-from findInPage.js */
 /* import-globals-from /browser/base/content/utilityOverlay.js */
 /* import-globals-from /toolkit/content/preferencesBindings.js */
@@ -508,6 +509,7 @@ function init_all() {
   register_module("paneSearch", gSearchPane);
   register_module("panePrivacy", gPrivacyPane);
   register_module("paneContainers", gContainersPane);
+  register_module("paneR3dfox", gR3dfoxPane);
 
   // Restore the cached Firefox Labs nav button visibility so it shows
   // immediately when recipes are expected to be available, before
