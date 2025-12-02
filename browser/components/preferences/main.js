da7a6c499560160e2cc778688c3e9ca593bbf609
the_r3dacted <k4sum1@eclipse.cx>
2025-12-02 05:57:52 -0600

    Bug 1979338 - Use an <html:img> rather than <xul:image> for menu icons. r=Gijs,desktop-theme-reviewers,settings-reviewers,tabbrowser-reviewers,sidebar-reviewers,sthompson,nsharpley,dao    2025-07-30 19:35:36    20d042e259898a4532d3ff680f7ac188749cdf6c

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -4008,7 +4008,6 @@ var gMainPane = {
         "action",
         Ci.nsIHandlerInfo.handleInternally
       );
-      internalMenuItem.className = "menuitem-iconic";
       document.l10n.setAttributes(internalMenuItem, "applications-open-inapp");
       internalMenuItem.setAttribute(APP_ICON_ATTR_NAME, "handleInternally");
       menuPopup.appendChild(internalMenuItem);
@@ -4016,7 +4015,6 @@ var gMainPane = {
 
     var askMenuItem = document.createXULElement("menuitem");
     askMenuItem.setAttribute("action", Ci.nsIHandlerInfo.alwaysAsk);
-    askMenuItem.className = "menuitem-iconic";
     document.l10n.setAttributes(askMenuItem, "applications-always-ask");
     askMenuItem.setAttribute(APP_ICON_ATTR_NAME, "ask");
     menuPopup.appendChild(askMenuItem);
@@ -4029,7 +4027,6 @@ var gMainPane = {
       saveMenuItem.setAttribute("action", Ci.nsIHandlerInfo.saveToDisk);
       document.l10n.setAttributes(saveMenuItem, "applications-action-save");
       saveMenuItem.setAttribute(APP_ICON_ATTR_NAME, "save");
-      saveMenuItem.className = "menuitem-iconic";
       menuPopup.appendChild(saveMenuItem);
     }
 
@@ -4062,10 +4059,10 @@ var gMainPane = {
             "app-name": handlerInfo.defaultDescription,
           }
         );
-        let image = handlerInfo.iconURLForSystemDefault;
-        if (image) {
-          defaultMenuItem.setAttribute("image", image);
-        }
+        defaultMenuItem.setAttribute(
+          "image",
+          handlerInfo.iconURLForSystemDefault
+        );
       }
 
       menuPopup.appendChild(defaultMenuItem);
@@ -4090,10 +4087,10 @@ var gMainPane = {
       document.l10n.setAttributes(menuItem, "applications-use-app", {
         "app-name": label,
       });
-      let image = this._getIconURLForHandlerApp(possibleApp);
-      if (image) {
-        menuItem.setAttribute("image", image);
-      }
+      menuItem.setAttribute(
+        "image",
+        this._getIconURLForHandlerApp(possibleApp)
+      );
 
       // Attach the handler app object to the menu item so we can use it
       // to make changes to the datastore when the user selects the item.
@@ -4127,11 +4124,10 @@ var gMainPane = {
           document.l10n.setAttributes(menuItem, "applications-use-app", {
             "app-name": handler.name,
           });
-
-          let image = this._getIconURLForHandlerApp(handler);
-          if (image) {
-            menuItem.setAttribute("image", image);
-          }
+          menuItem.setAttribute(
+            "image",
+            this._getIconURLForHandlerApp(handler)
+          );
 
           // Attach the handler app object to the menu item so we can use it
           // to make changes to the datastore when the user selects the item.
