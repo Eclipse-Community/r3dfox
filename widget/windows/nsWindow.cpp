6303cde77783aaea47acce1ea030ec0ef7f5f0ab
the_r3dacted <k4sum1@eclipse.cx>
2025-12-02 05:59:19 -0600

    Bug 1958711 - Remove reentrancy from nsWindow::OnPaint. r=win-reviewers,handyman    2025-04-16 06:55:00    f60002b09e1cc4ce2cb4b8268383620dd82427a8

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -5170,7 +5170,7 @@ bool nsWindow::ProcessMessageInternal(UINT msg, WPARAM& wParam, LPARAM& lParam,
       break;
 
     case WM_PAINT:
-      *aRetValue = (int)OnPaint();
+      *aRetValue = (int)OnPaint(0);
       result = true;
       break;
 
