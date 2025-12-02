80705d3bcbdd11e0c3d0967de4eab64d4f619040
the_r3dacted <k4sum1@eclipse.cx>
2025-12-02 06:06:45 -0600

    Bug 1845746 - Remove gfxWindowsPlatform::DwmCompositionEnabled. r=gfx-reviewers,jrmuizel    2023-08-11 02:58:03    14c50bb16d4f0ca5159f4f1534241ab1ab7b354d

--- gfx/thebes/gfxWindowsPlatform.h
+++ gfx/thebes/gfxWindowsPlatform.h
@@ -150,6 +150,8 @@ class gfxWindowsPlatform final : public gfxPlatform {
  public:
   static nsresult GetGpuTimeSinceProcessStartInMs(uint64_t* aResult);
 
+  bool DwmCompositionEnabled();
+
   static bool IsOptimus();
 
   bool SupportsApzWheelInput() const override { return true; }
@@ -192,6 +194,12 @@ class gfxWindowsPlatform final : public gfxPlatform {
   BackendPrefsData GetBackendPrefs() const override;
 
  private:
+  enum class DwmCompositionStatus : uint32_t {
+    Unknown,
+    Disabled,
+    Enabled,
+  };
+
   void Init();
   void InitAcceleration() override;
   void InitWebRenderConfig() override;
@@ -218,6 +226,9 @@ class gfxWindowsPlatform final : public gfxPlatform {
   void RecordStartupTelemetry();
 
   bool mInitializedDevices = false;
+
+  mozilla::Atomic<DwmCompositionStatus, mozilla::ReleaseAcquire>
+      mDwmCompositionStatus;
 
   // Cached contents of the output color profile file
   nsTArray<uint8_t> mCachedOutputColorProfile;
