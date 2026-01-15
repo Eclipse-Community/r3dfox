c93a565985afacf5a3940467a69990dbbddf9512
Andrew Osmond <aosmond@gmail.com>
2026-01-15 13:22:28 +0000

    Bug 1976929 - Don't assume MediaChangeMonitor::mDecodePromise is set after dispatching.  a=dmeehan DONTBUILD
    Since the owner can call MediaChangeMonitor::Shutdown and reject any
    outstanding promises, we shouldn't assume mDecodePromise will still be
    set in any promise chains after dispatching. This will allow us to
    shutdown gracefully.
    
    Differential Revision: https://phabricator.services.mozilla.com/D278127

--- dom/media/platforms/wrappers/MediaChangeMonitor.cpp
+++ dom/media/platforms/wrappers/MediaChangeMonitor.cpp
@@ -912,7 +912,7 @@ MediaResult MediaChangeMonitor::CreateDecoderAndInit(MediaRawData* aSample) {
                         return;
                       }
 
-                      mDecodePromise.Reject(
+                      mDecodePromise.RejectIfExists(
                           MediaResult(
                               aError.Code(),
                               RESULT_DETAIL("Unable to initialize decoder")),
@@ -927,7 +927,7 @@ MediaResult MediaChangeMonitor::CreateDecoderAndInit(MediaRawData* aSample) {
               mFlushPromise.Reject(aError, __func__);
               return;
             }
-            mDecodePromise.Reject(
+            mDecodePromise.RejectIfExists(
                 MediaResult(NS_ERROR_DOM_MEDIA_FATAL_ERR,
                             RESULT_DETAIL("Unable to create decoder")),
                 __func__);
@@ -971,12 +971,12 @@ void MediaChangeMonitor::DecodeFirstSample(MediaRawData* aSample) {
           [self, this](MediaDataDecoder::DecodedData&& aResults) {
             mDecodePromiseRequest.Complete();
             mPendingFrames.AppendElements(std::move(aResults));
-            mDecodePromise.Resolve(std::move(mPendingFrames), __func__);
+            mDecodePromise.ResolveIfExists(std::move(mPendingFrames), __func__);
             mPendingFrames = DecodedData();
           },
           [self, this](const MediaResult& aError) {
             mDecodePromiseRequest.Complete();
-            mDecodePromise.Reject(aError, __func__);
+            mDecodePromise.RejectIfExists(aError, __func__);
           })
       ->Track(mDecodePromiseRequest);
 }
@@ -1035,7 +1035,7 @@ void MediaChangeMonitor::DrainThenFlushDecoder(MediaRawData* aPendingSample) {
               mFlushPromise.Reject(aError, __func__);
               return;
             }
-            mDecodePromise.Reject(aError, __func__);
+            mDecodePromise.RejectIfExists(aError, __func__);
           })
       ->Track(mDrainRequest);
 }
@@ -1079,7 +1079,7 @@ void MediaChangeMonitor::FlushThenShutdownDecoder(
                         return;
                       }
                       MOZ_ASSERT(NS_FAILED(rv));
-                      mDecodePromise.Reject(rv, __func__);
+                      mDecodePromise.RejectIfExists(rv, __func__);
                       return;
                     },
                     [] { MOZ_CRASH("Can't reach here'"); })
@@ -1092,7 +1092,7 @@ void MediaChangeMonitor::FlushThenShutdownDecoder(
               mFlushPromise.Reject(aError, __func__);
               return;
             }
-            mDecodePromise.Reject(aError, __func__);
+            mDecodePromise.RejectIfExists(aError, __func__);
           })
       ->Track(mFlushRequest);
 }
