93d14314646aa7d5e9d21b7fedd0daac97523936
Brad Werth <werth@efn.org>
2026-01-16 17:20:58 +0000

    Bug 1972888 Part 3: Maintain the retain count of the nsCocoaWindow native window during runloops.  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D272538
    
    Differential Revision: https://phabricator.services.mozilla.com/D278726

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -376,13 +376,9 @@ class nsCocoaWindow final : public nsBaseWidget {
   void UpdateFullscreenState(bool aFullScreen, bool aNativeMode);
   nsresult DoMakeFullScreen(bool aFullScreen, bool aUseSystemTransition);
 
-  already_AddRefed<nsIWidget> AllocateChildPopupWidget() override {
-    return nsIWidget::CreateTopLevelWindow();
-  }
-
   nsIWidget* mParent;        // if we're a popup, this is our parent [WEAK]
   nsIWidget* mAncestorLink;  // link to traverse ancestors [WEAK]
-  BaseWindow* mWindow;                // our cocoa window [STRONG]
+  BaseWindow* mWindow;       // our cocoa window [STRONG]
   // window upon closing it, held through our destructor. This is useful
   // to ensure that macOS run loops which reference the window will still
   // have something to point to even if they don't use proper retain and
