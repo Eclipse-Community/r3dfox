c79d9ff5f8c1450e1c570ee3ea4e9dc66a47d242
the_r3dacted <k4sum1@eclipse.cx>
2026-01-18 20:25:46 -0600

    Bug 1845636 - Allow browser chrome to enable the non-native theme. r=dshin    2023-07-28 06:09:10    f9e8ec8dc66648a94d75418ebfebaebf3dbad256

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -3130,7 +3130,6 @@ LogicalSide nsStyleText::TextEmphasisSide(WritingMode aWM,
 
 nsStyleUI::nsStyleUI()
     : mInert(StyleInert::None),
-      mMozTheme(StyleMozTheme::Auto),
       mUserFocus(StyleUserFocus::Normal),
       mPointerEvents(StylePointerEvents::Auto),
       mCursor{{}, StyleCursorKind::Auto},
@@ -3143,7 +3142,6 @@ nsStyleUI::nsStyleUI()
 
 nsStyleUI::nsStyleUI(const nsStyleUI& aSource)
     : mInert(aSource.mInert),
-      mMozTheme(aSource.mMozTheme),
       mUserFocus(aSource.mUserFocus),
       mPointerEvents(aSource.mPointerEvents),
       mCursor(aSource.mCursor),
@@ -3203,7 +3201,6 @@ nsChangeHint nsStyleUI::CalcDifference(const nsStyleUI& aNewData) const {
   if (mCaretColor != aNewData.mCaretColor ||
       mAccentColor != aNewData.mAccentColor ||
       mScrollbarColor != aNewData.mScrollbarColor ||
-      mMozTheme != aNewData.mMozTheme ||
       mColorScheme != aNewData.mColorScheme) {
     hint |= nsChangeHint_RepaintFrame;
   }
