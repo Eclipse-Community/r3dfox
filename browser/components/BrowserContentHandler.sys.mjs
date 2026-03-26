ecc31d13344a04794445c64aece342166654bcb7
the_r3dacted <k4sum1@eclipse.cx>
2026-03-26 04:57:36 -0500

    Bug 1846845 - Remove some unneeded isPlatformAndVersionAtLeast checks r=Gijs    2023-08-03 06:30:37    37dc90e72c67db0c1ee23b88a1b17cb3636b3ab7

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -1649,7 +1649,7 @@ nsDefaultCommandLineHandler.prototype = {
       }
     } else if (!cmdLine.preventDefault) {
       if (
-        AppConstants.platform == "win" &&
+        AppConstants.isPlatformAndVersionAtLeast("win", "10") &&
         cmdLine.state != Ci.nsICommandLine.STATE_INITIAL_LAUNCH &&
         lazy.WindowsUIUtils.inWin10TabletMode
       ) {
