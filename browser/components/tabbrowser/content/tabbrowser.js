4244b4280bcb0c16ca015ff4a8dc0ed5b0b03e84
the_r3dacted <k4sum1@eclipse.cx>
2026-03-26 11:14:24 -0500

    I SPECIFICALLY MODIFIED THE PATCH TO INCLUDE THIS WHY IS IT NOT APPLIED?!?!?!?

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -10004,7 +10004,7 @@ var TabBarVisibility = {
       .getElementById("browser")
       .classList.toggle(
         "browser-toolbox-background",
-        CustomTitlebar.enabled && hasVerticalTabs
+        TabsInTitlebar.enabled && hasVerticalTabs
       );
 
     if (
