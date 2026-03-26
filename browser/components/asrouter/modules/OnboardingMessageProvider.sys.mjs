a70bc49dc51286a7a4be0fd04cededecb801e563
the_r3dacted <k4sum1@eclipse.cx>
2026-03-26 10:26:07 -0500

    hide-finish-setup-bookmark.patch from Librewolf

--- browser/components/asrouter/modules/OnboardingMessageProvider.sys.mjs
+++ browser/components/asrouter/modules/OnboardingMessageProvider.sys.mjs
@@ -2857,7 +2857,7 @@ const BASE_MESSAGES = () => [
     },
     priority: 3,
     targeting:
-      "'messaging-system-action.easyChecklist.open' | preferenceValue == true",
+      "false",
     trigger: {
       id: "preferenceObserver",
       params: ["messaging-system-action.easyChecklist.open"],
@@ -3061,7 +3061,7 @@ const BASE_MESSAGES = () => [
     },
     priority: 3,
     targeting:
-      "'messaging-system-action.easyChecklist.open' | preferenceValue == true",
+      "false",
     trigger: {
       id: "messagesLoaded",
     },
