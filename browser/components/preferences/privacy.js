bdd5968364237189a1c0ddbe375211c2945dbdad
the_r3dacted <k4sum1@eclipse.cx>
2026-03-26 10:22:48 -0500

    webgl-permission.patch from Librewolf

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -1604,6 +1604,16 @@ var gPrivacyPane = {
     PrivacySettingHelpers.showXRExceptions();
   },
 
+  // WebGL
+
+  /**
+  * Displays the WebGL exceptions dialog where specific site WebGL
+  * preferences can be set.
+  */
+  showWebGLExceptions() {
+    PrivacySettingHelpers.showWebGLExceptions();
+  },
+
   // CAMERA
 
   /**
