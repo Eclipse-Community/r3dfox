0cd9fbc8e0e7233910196dffbac0a14832daae7a
the_r3dacted <k4sum1@eclipse.cx>
2026-03-26 10:22:48 -0500

    webgl-permission.patch from Librewolf
    Librewolf ownerGlobal replaced stuffs
    
    webgl-permission.patch English ftl localization
    
    Librewolf has a stupid localization system that keeps this separate from the patch.
    
    move webgl perf to hopefully make it work

--- browser/components/preferences/config/permissions-data.mjs
+++ browser/components/preferences/config/permissions-data.mjs
@@ -57,6 +57,9 @@ Preferences.addAll([
 
   { id: PREF_NORMANDY_ENABLED, type: "bool" },
   { id: "nimbus.rollouts.enabled", type: "bool" },
+
+  // WebGL permissions
+  { id: "webgl.disabled", type: "bool" },
 ]);
 
 // Study opt out
@@ -200,6 +203,20 @@ Preferences.addSetting({
   id: "xrSettingsButton",
   onUserClick: () => showPermissionExceptions({ permissionType: "xr" }),
 });
+
+Preferences.addSetting({
+  id: "webglDisabled",
+  pref: "webgl.disabled",
+});
+Preferences.addSetting({
+  id: "webglSettingsButton",
+  onUserClick: () => showPermissionExceptions({ permissionType: "webgl" }),
+  deps: ["webglDisabled"],
+  visible: deps => {
+    return !deps.webglDisabled.value;
+  },
+});
+
 Preferences.addSetting({
   id: "popupPolicy",
   pref: "dom.disable_open_during_load",
@@ -600,6 +617,16 @@ SettingGroupManager.registerGroups({
               ".iconSrc": "chrome://browser/skin/notification-icons/xr.svg",
               "search-l10n-ids":
                 "permissions-remove.label,permissions-remove-all.label,permissions-site-xr-window2.title,permissions-site-xr-desc,permissions-site-xr-disable-label,permissions-site-xr-disable-desc,",
+            },
+          },
+          {
+            id: "webglSettingsButton",
+            control: "moz-box-button",
+            l10nId: "permissions-webgl2",
+            controlAttrs: {
+              ".iconSrc": "chrome://browser/skin/notification-icons/webgl.svg",
+              "search-l10n-ids":
+               "permissions-remove.label,permissions-remove-all.label,permissions-site-webgl-window2.title,permissions-site-webgl-desc",
             },
           },
         ],
