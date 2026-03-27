db8d6d6a7b57ffafda448713987f813836bfb0a2
the_r3dacted <k4sum1@eclipse.cx>
2026-03-27 10:57:21 -0500

    Extend JPEG XL detection/recognition from Waterfox
    The credit for this one is weird. The modern day commit mentions https://github.com/louie-github but the only pr I can find relating to this is from https://github.com/goodusername123 however his version is quite different from the modern day one. The ESR 128 version of this commit only mentions https://github.com/MrAlex94 so that's who I've given credit for this.
    
    Removed Rust overrides since the suggestion in eRust should* take care of it and I hate having it break every so often. fuck cargo
    
    Co-Authored-By: Alex Kontos <4728920+mralex94@users.noreply.github.com>

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -1310,6 +1310,7 @@ function maybeRecordToHandleTelemetry(uri, isLaunch) {
       ".xhtml",
       ".svg",
       ".webp",
+      ".jxl",
     ]);
     if (registeredExtensions.has(extension)) {
       counter[extension].add(1);
