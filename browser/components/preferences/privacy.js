17912e720cca2b1948bff92c52aadaab6a577a9a
the_r3dacted <k4sum1@eclipse.cx>
2026-03-27 09:02:07 -0500

    Add back useful privacy settings

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -3506,7 +3506,7 @@ var gPrivacyPane = {
     initSettingGroup("securityPrivacyStatus");
     initSettingGroup("securityPrivacyWarnings");
     initSettingGroup("httpsOnly");
-    //initSettingGroup("browsingProtection");
+    initSettingGroup("browsingProtection");
     initSettingGroup("cookiesAndSiteData");
     initSettingGroup("cookiesAndSiteData2");
     initSettingGroup("certificates");
