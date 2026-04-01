0435f7b37384442277ed464766a99e76956ec812
Valentin Gosu <valentin.gosu@gmail.com>
2026-04-01 07:55:08 +0000

    Bug 2022294 - FetchParent::RecvFetchOp should only be called once  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D288154
    
    Differential Revision: https://phabricator.services.mozilla.com/D290440

--- dom/fetch/FetchParent.h
+++ dom/fetch/FetchParent.h
@@ -101,6 +101,7 @@ class FetchParent final : public PFetchParent {
 
   Atomic<bool> mIsDone{false};
   Atomic<bool> mActorDestroyed{false};
+  Atomic<bool> mReceivedFetchOp{false};
 
   nsCOMPtr<nsISerialEventTarget> mBackgroundEventTarget;
 };
