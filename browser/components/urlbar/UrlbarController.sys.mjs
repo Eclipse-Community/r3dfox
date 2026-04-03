f09ee3462786bf384e74e3c98c544afa38c6418e
the_r3dacted <k4sum1@eclipse.cx>
2026-04-03 17:57:46 -0500

    Better version of urlbarprovider-interventions.patch

--- browser/components/urlbar/UrlbarController.sys.mjs
+++ browser/components/urlbar/UrlbarController.sys.mjs
@@ -1065,6 +1065,11 @@ class TelemetryEvent {
       selIndex: details.result?.rowIndex ?? -1,
     };
 
+    // Skip telemetry for intervention tips
+    if (details.provider == "UrlbarProviderInterventions") {
+      return;
+    }
+
     let { queryContext } = this._controller._lastQueryContextWrapper || {};
 
     this.#recordSearchEngagementTelemetry(method, startEventInfo, {
