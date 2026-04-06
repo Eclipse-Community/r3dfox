fb0e81dd0da57d291e5a255f3704bc336e385817
Randell Jesup <rjesup@mozilla.com>
2026-04-06 20:23:27 +0000

    Bug 2024478: Add a death grip  a=RyanVM DONTBUILD
    esr115 version
    
    Differential Revision: https://phabricator.services.mozilla.com/D290616

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -2495,6 +2495,7 @@ void XMLHttpRequestMainThread::ChangeStateToDone(bool aWasSync) {
 
 void XMLHttpRequestMainThread::ChangeStateToDoneInternal() {
   DEBUG_WORKERREFS;
+  RefPtr<XMLHttpRequestMainThread> kungfuDeathGrip(this);
   DisconnectDoneNotifier();
   StopProgressEventTimer();
 
