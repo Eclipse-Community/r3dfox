c5e39893232d5b6121982450fc926a5a73cd6669
Brad Werth <bwerth@mozilla.com>
2026-04-06 18:20:32 +0000

    Bug 2023276 Part 2: Early exit BufferTextureHost::GetAsSurface when buffer is null.  a=dmeehan,RyanVM
    Original Revision: https://phabricator.services.mozilla.com/D288734
    
    Differential Revision: https://phabricator.services.mozilla.com/D290598

--- gfx/layers/composite/TextureHost.cpp
+++ gfx/layers/composite/TextureHost.cpp
@@ -708,7 +708,11 @@ already_AddRefed<gfx::DataSourceSurface> BufferTextureHost::GetAsSurface(
   if (mFormat == gfx::SurfaceFormat::UNKNOWN) {
     NS_WARNING("BufferTextureHost: unsupported format!");
     return nullptr;
-  } else if (mFormat == gfx::SurfaceFormat::YUV) {
+  }
+  if (!GetBuffer()) {
+    return nullptr;
+  }
+  if (mFormat == gfx::SurfaceFormat::YUV) {
     result = ImageDataSerializer::DataSourceSurfaceFromYCbCrDescriptor(
         GetBuffer(), mDescriptor.get_YCbCrDescriptor(), aSurface);
     if (NS_WARN_IF(!result)) {
