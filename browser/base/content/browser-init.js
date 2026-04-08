a30891f6f9fc3e941f9f8fa7c6d8fbb7dbdb4d77
the_r3dacted <k4sum1@eclipse.cx>
2026-04-08 01:57:09 -0500

    Prepare for MOZILLA_OFFICIAL from Librewolf
    autoconfig-setEnv.patch from Librewolf
    moz-official.patch from Librewolf

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -671,7 +671,7 @@ var gBrowserInit = {
       gDataNotificationInfoBar.init();
     }
 
-    if (!AppConstants.MOZILLA_OFFICIAL) {
+    if (Services.prefs.getBoolPref("librewolf.devHelpers", false)) {
       DevelopmentHelpers.init();
     }
 
