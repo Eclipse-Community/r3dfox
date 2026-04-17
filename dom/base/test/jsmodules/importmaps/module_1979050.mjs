erichdongubler-mozilla/firefox

Bug 1979050 - Part 2: test(ESR 115).  a=diannaS
Differential Revision: https://phabricator.services.mozilla.com/D272648

dom/base/test/jsmodules/importmaps/module_1979050.mjs
@@ -1,0 +1,3 @@
+import { x } from "./module_simpleExport.js";
+
+result = x;

dom/base/test/jsmodules/importmaps/scope1/module_1979050.mjs
@@ -1,0 +1,5 @@
+import { x } from "./module_simpleExport.js";
+
+result2 = x;
+
+export let y = x;

dom/base/test/jsmodules/importmaps/scope1/scope2/module_1979050.mjs
@@ -1,0 +1,5 @@
+import { x } from "./module_simpleExport.js";
+
+result3 = x;
+
+export let z = x;

Eclipse-Community/r3dfox

dom/base/test/jsmodules/importmaps/module_1979050.mjs
@@ -1,0 +1,3 @@
+import { x } from "./module_simpleExport.mjs";
+
+result = x;

dom/base/test/jsmodules/importmaps/scope1/module_1979050.mjs
@@ -1,0 +1,5 @@
+import { x } from "./module_simpleExport.mjs";
+
+result2 = x;
+
+export let y = x;

dom/base/test/jsmodules/importmaps/scope1/scope2/module_1979050.mjs
@@ -1,0 +1,5 @@
+import { x } from "./module_simpleExport.mjs";
+
+result3 = x;
+
+export let z = x;