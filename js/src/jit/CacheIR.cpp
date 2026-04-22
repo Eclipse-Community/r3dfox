ba4d62945242183d37a4dc9f5bd315295629baa8
the_r3dacted <k4sum1@eclipse.cx>
2026-04-22 13:33:01 -0500

    Revert "Bug 1998050 - Check for typed array index in canAttachAddSlotStub.  a=diannaS"
    This reverts commit 276abdd403d5ea0c5df192c9cf7c86e647d6185b.

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -5440,13 +5440,12 @@ bool SetPropIRGenerator::canAttachAddSlotStub(HandleObject obj, HandleId id) {
       return false;
     }
   } else {
-    // Normal Case: If property exists or is an OOB typed array index, this
-    // isn't an "add".
+    // Normal Case: If property exists this isn't an "add"
     PropertyResult prop;
     if (!LookupOwnPropertyPure(cx_, nobj, id, &prop)) {
       return false;
     }
-    if (prop.isFound() || prop.isTypedArrayOutOfRange()) {
+    if (prop.isFound()) {
       return false;
     }
   }
@@ -5550,10 +5549,6 @@ AttachDecision SetPropIRGenerator::tryAttachAddSlotStub(
   }
 
   JSObject* obj = &lhsVal_.toObject();
-  if (!obj->is<NativeObject>()) {
-    return AttachDecision::NoAction;
-  }
-  NativeObject* nobj = &obj->as<NativeObject>();
 
   PropertyResult prop;
   if (!LookupOwnPropertyPure(cx_, obj, id, &prop)) {
@@ -5563,7 +5558,11 @@ AttachDecision SetPropIRGenerator::tryAttachAddSlotStub(
     return AttachDecision::NoAction;
   }
 
-  MOZ_RELEASE_ASSERT(prop.isNativeProperty());
+  if (!obj->is<NativeObject>()) {
+    return AttachDecision::NoAction;
+  }
+  auto* nobj = &obj->as<NativeObject>();
+
   PropertyInfo propInfo = prop.propertyInfo();
   NativeObject* holder = nobj;
 
@@ -5575,7 +5574,6 @@ AttachDecision SetPropIRGenerator::tryAttachAddSlotStub(
 
   // The property must be the last added property of the object.
   SharedShape* newShape = holder->sharedShape();
-  MOZ_RELEASE_ASSERT(oldShape != newShape);
   MOZ_RELEASE_ASSERT(newShape->lastProperty() == propInfo);
 
 #ifdef DEBUG
