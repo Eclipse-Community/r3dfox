1bb041f4f52d9b12f3b0f33bee6180a3d31f9a19
the_r3dacted <k4sum1@eclipse.cx>
2026-05-01 12:02:31 -0500

    Bug 1856104 - Remove useless version checks from JavaScript code. r=Gijs,webcompat-reviewers,settings-reviewers,search-reviewers,Standard8,twisniewski    2023-10-03 03:00:07    8262afb8c6e4bade9230df876e8c5e8cb8cc3eaf

--- browser/components/shell/ShellService.sys.mjs
+++ browser/components/shell/ShellService.sys.mjs
@@ -9,6 +9,7 @@ const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
   NimbusFeatures: "resource://nimbus/ExperimentAPI.sys.mjs",
+  WindowsRegistry: "resource://gre/modules/WindowsRegistry.sys.mjs",
   ASRouter: "resource:///modules/asrouter/ASRouter.sys.mjs",
   WindowsVersionInfo:
     "resource://gre/modules/components-utils/WindowsVersionInfo.sys.mjs",
@@ -115,6 +116,26 @@ let ShellServiceInternal = {
     );
   },
 
+  isDefaultBrowserOptOut() {
+    if (AppConstants.platform == "win") {
+      let optOutValue = lazy.WindowsRegistry.readRegKey(
+        Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+        "Software\\Mozilla\\Firefox",
+        "DefaultBrowserOptOut"
+      );
+      lazy.WindowsRegistry.removeRegKey(
+        Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+        "Software\\Mozilla\\Firefox",
+        "DefaultBrowserOptOut"
+      );
+      if (optOutValue == "True") {
+        Services.prefs.setBoolPref("browser.shell.checkDefaultBrowser", false);
+        return true;
+      }
+    }
+    return false;
+  },
+
   /**
    * Used to determine whether or not to show a "Set Default Browser"
    * query dialog. This attribute is true if the application is starting
@@ -130,6 +151,10 @@ let ShellServiceInternal = {
     }
 
     if (!Services.prefs.getBoolPref("browser.shell.checkDefaultBrowser")) {
+      return false;
+    }
+
+    if (this.isDefaultBrowserOptOut()) {
       return false;
     }
 
@@ -369,9 +394,9 @@ let ShellServiceInternal = {
 
   // override nsIShellService.setDefaultBrowser() on the ShellService proxy.
   async setDefaultBrowser(forAllUsers) {
-    // On Windows, our best chance is to set UserChoice, so try that first.
+    // On Windows 10, our best chance is to set UserChoice, so try that first.
     if (
-      AppConstants.platform == "win" &&
+      AppConstants.isPlatformAndVersionAtLeast("win", "10") &&
       Services.prefs.getBoolPref("browser.shell.setDefaultBrowserUserChoice")
     ) {
       try {
@@ -414,7 +439,7 @@ let ShellServiceInternal = {
   },
 
   async setAsDefaultPDFHandler(onlyIfKnownBrowser = false) {
-    if (AppConstants.platform != "win") {
+    if (!AppConstants.isPlatformAndVersionAtLeast("win", "10")) {
       throw new Error("Windows-only");
     }
 
