28296bb1ff5fad4c1f99226261f7b8b658b7cd78
the_r3dacted <k4sum1@eclipse.cx>
2026-05-01 11:58:44 -0500

    Bug 1934375 - Take over whole non-client area for PiP windows again, for now. r=win-reviewers,gstoll    2025-01-31 02:36:09    39c490cb6d24a650d294eaa7ad7da037d1b1bdd5

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -2835,9 +2835,6 @@ bool nsWindow::UpdateNonClientMargins(bool aReflowWindow) {
         }
       }
     }
-  } else if (mPiPType == PiPType::MediaPiP &&
-             !StaticPrefs::widget_windows_pip_decorations_enabled()) {
-    metrics.mOffset = metrics.DefaultMargins();
   } else {
     metrics.mOffset = NormalWindowNonClientOffset();
   }
