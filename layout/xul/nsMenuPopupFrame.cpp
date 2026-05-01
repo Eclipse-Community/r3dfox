194f50b2e3a1c181117d1edfd7f688921f6278e8
the_r3dacted <k4sum1@eclipse.cx>
2026-05-01 11:55:24 -0500

    Bug 2002232 - Clean up top level widget painting. r=jwatt    2025-11-27 16:49:59    d7598a4a4a878f626044056ba651d3dc3d1e2d8c

--- layout/xul/nsMenuPopupFrame.cpp
+++ layout/xul/nsMenuPopupFrame.cpp
@@ -2592,7 +2592,7 @@ nsEventStatus nsMenuPopupFrame::HandleEvent(mozilla::WidgetGUIEvent* aEvent) {
   return status;
 }
 
-void nsMenuPopupFrame::PaintWindow(nsIWidget* aWidget) {
+bool nsMenuPopupFrame::PaintWindow(nsIWidget* aWidget, LayoutDeviceIntRegion) {
   MOZ_ASSERT(aWidget == mWidget);
   nsAutoScriptBlocker scriptBlocker;
   RefPtr ps = PresShell();
@@ -2602,6 +2602,7 @@ void nsMenuPopupFrame::PaintWindow(nsIWidget* aWidget) {
   } else {
     ps->SyncPaintFallback(this, renderer);
   }
+  return true;
 }
 
 void nsMenuPopupFrame::DidCompositeWindow(
