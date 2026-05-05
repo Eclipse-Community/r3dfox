ce95d7d77472d334c07b1784d953bb77eb713191
the_r3dacted <k4sum1@eclipse.cx>
2026-05-05 00:37:31 -0700

    Revert "Bug 1615921 - Suspend late write checks in Windows vprintf_stderr r=froydnj"
    This reverts commit e1f74d8202093e4372dcd2dc31104e8a3ad895f4.

--- xpcom/base/Logging.cpp
+++ xpcom/base/Logging.cpp
@@ -12,7 +12,6 @@
 #include "GeckoProfiler.h"
 #include "mozilla/ClearOnShutdown.h"
 #include "mozilla/FileUtils.h"
-#include "mozilla/LateWriteChecks.h"
 #include "mozilla/Mutex.h"
 #include "mozilla/StaticPtr.h"
 #include "mozilla/Printf.h"
@@ -589,7 +588,6 @@ class LogModuleManager {
   void Print(const char* aName, LogLevel aLevel, const TimeStamp* aStart,
              const char* aPrepend, const char* aFmt, va_list aArgs)
       MOZ_FORMAT_PRINTF(6, 0) {
-    AutoSuspendLateWriteChecks suspendLateWriteChecks;
     long pid = static_cast<long>(base::GetCurrentProcId());
     const size_t kBuffSize = 1024;
     char buff[kBuffSize];
