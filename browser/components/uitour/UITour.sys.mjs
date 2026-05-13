b207139c237dcd7003c95e5be76ccb9897b4637c
the_r3dacted <k4sum1@eclipse.cx>
2026-05-13 18:41:59 -0500

    Bug 1845057 - Remove aClaimAllTypes from setDefaultBrowser. r=nrishel,settings-reviewers    2023-10-03 00:56:42    c2dc4361260c3e550fb34c942d29ce5be4b3a856

--- browser/components/uitour/UITour.sys.mjs
+++ browser/components/uitour/UITour.sys.mjs
@@ -1683,7 +1683,7 @@ export var UITour = {
         try {
           let shell = aWindow.getShellService();
           if (shell) {
-            await shell.setDefaultBrowser(false);
+            await shell.setDefaultBrowser(true, false);
           }
         } catch (e) {}
         break;
