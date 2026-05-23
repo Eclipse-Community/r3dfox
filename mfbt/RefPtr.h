792fdbce3b95049c5335da1873299af65fdda7de
the_r3dacted <k4sum1@eclipse.cx>
2026-05-22 22:41:07 -0500

    Bug 1869053 - [1/5] modernize our `mscom::AgileReference` class  r=handyman    2023-12-22 03:46:54    1b1974da411775fd82b432fb299860937dcbabc1 Bug 1869053 - [2/5] eliminate all implicit conversions between RefPtr and AgileReference  r=handyman,win-reviewers    2023-12-22 03:46:54    a7b8bc44e8e2b4e605231de8fb5de6ccb7c22c18 Bug 1869053 - [3/5] simplify use of AgileReference in IAudioSessionControl  r=handyman,win-reviewers    2023-12-22 03:46:55    f4663273911fc66f80b62548e90d4871fd89cd2e Bug 1869053 - [4/5] remove EnsureMTA::CreateInstance  r=handyman    2023-12-22 03:46:55    e1e150d3f1765112f645f7aaa2ffcfe949e7bf8d Bug 1869053 - [5/5] templatize and further simplify AgileReference  r=handyman,win-reviewers    2023-12-22 03:46:55    46a0bdbeb689532f1375b48712af8911e4c65a3a

--- mfbt/RefPtr.h
+++ mfbt/RefPtr.h
@@ -31,6 +31,11 @@ template <class T>
 class StaticLocalRefPtr;
 template <class T>
 class StaticRefPtr;
+#if defined(XP_WIN)
+namespace mscom {
+class AgileReference;
+}  // namespace mscom
+#endif  // defined(XP_WIN)
 
 // Traditionally, RefPtr supports automatic refcounting of any pointer type
 // with AddRef() and Release() methods that follow the traditional semantics.
@@ -159,6 +164,9 @@ class MOZ_IS_REFPTR MOZ_NULL_AFTER_MOVE RefPtr {
 
   MOZ_IMPLICIT RefPtr(const nsQueryReferent& aHelper);
   MOZ_IMPLICIT RefPtr(const nsCOMPtr_helper& aHelper);
+#if defined(XP_WIN)
+  MOZ_IMPLICIT RefPtr(const mozilla::mscom::AgileReference& aAgileRef);
+#endif  // defined(XP_WIN)
 
   // Defined in OwningNonNull.h
   template <class U>
@@ -219,6 +227,9 @@ class MOZ_IS_REFPTR MOZ_NULL_AFTER_MOVE RefPtr {
 
   RefPtr<T>& operator=(const nsQueryReferent& aQueryReferent);
   RefPtr<T>& operator=(const nsCOMPtr_helper& aHelper);
+#if defined(XP_WIN)
+  RefPtr<T>& operator=(const mozilla::mscom::AgileReference& aAgileRef);
+#endif  // defined(XP_WIN)
 
   template <typename I,
             typename = std::enable_if_t<std::is_convertible_v<I*, T*>>>
