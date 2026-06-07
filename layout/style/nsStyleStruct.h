d5825d530e593a93505c413d0ec6a6a59f0020a2
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:13:16 -0500

    Bug 1845636 - Allow browser chrome to enable the non-native theme. r=dshin    2023-07-28 06:09:10    f9e8ec8dc66648a94d75418ebfebaebf3dbad256

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -2193,7 +2193,6 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleUI {
   void TriggerImageLoads(mozilla::dom::Document&, const nsStyleUI*);
 
   mozilla::StyleInert mInert;
-  mozilla::StyleMozTheme mMozTheme;
 
  private:
   mozilla::StyleUserFocus mUserFocus;
