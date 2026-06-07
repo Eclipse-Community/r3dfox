8448d27b2cb58c3e3c0aee2dbc49e749381eedec
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:11:13 -0500

    Bug 1861605 - Use IDXGIResource1::CreateSharedHandle() instead of IDXGIResource::GetSharedHandle() r=gfx-reviewers,lsalzman    2023-12-19 17:23:21    5e4ff4aeaeda75735743446987ab2f637ec780f4

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -5809,7 +5809,7 @@ bool ValidSurfaceDescriptorForRemoteCanvas2d(
             .get_SurfaceDescriptorRemoteDecoder()
             .subdesc()
             .get_SurfaceDescriptorD3D10()
-            .handle() = nullptr;
+            .handle() = 0;
       } else if (ssd.handle()) {
         return false;
       }
