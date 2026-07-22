f3a884d06c08f86af2955959d3bae982a2b419c8
the_r3dacted <k4sum1@eclipse.cx>
2026-07-22 16:06:19 -0500

    Disable tab unloading on low memory by default for now

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1132,11 +1132,7 @@ pref("security.allow_parent_unrestricted_js_loads", false);
 pref("browser.unexpectedScriptLoad.logLevel", "Warn");
 
 // Unload tabs when available memory is running low
-#if defined(XP_MACOSX) || defined(XP_WIN)
-    pref("browser.tabs.unloadOnLowMemory", true);
-#else
-    pref("browser.tabs.unloadOnLowMemory", false);
-#endif
+pref("browser.tabs.unloadOnLowMemory", false);
 
 // Tab Unloader does not unload tabs whose last inactive period is longer than
 // this value (in milliseconds).
