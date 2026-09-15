2dc9d5bbf675c7a96b53ec05e666bd4ad5370e0f
the_r3dacted <k4sum1@eclipse.cx>
2026-09-15 12:53:52 -0500

    Revert "Bug 2054673.  a=diannaS DONTBUILD"
    This reverts commit 56d6a58ab54d6f961c80eee91a00552f5d80444f.

--- gfx/layers/wr/WebRenderBridgeParent.cpp
+++ gfx/layers/wr/WebRenderBridgeParent.cpp
@@ -1130,17 +1130,9 @@ mozilla::ipc::IPCResult WebRenderBridgeParent::RecvDeleteCompositorAnimations(
       wr::AsUint64(mPipelineId), wr::AsUint64(mLateInit->mApi->GetId()),
       IsRootWebRenderBridgeParent());
 
-  nsTArray<uint64_t> ids;
-  ids.SetCapacity(aIds.Length());
-  for (uint64_t id : aIds) {
-    if (OwnsCompositorAnimationsId(id)) {
-      ids.AppendElement(id);
-    }
-  }
-
   // Once mWrEpoch has been rendered, we can delete these compositor animations
   mCompositorAnimationsToDelete.push(
-      CompositorAnimationIdsForEpoch(mWrEpoch, std::move(ids)));
+      CompositorAnimationIdsForEpoch(mWrEpoch, std::move(aIds)));
   return IPC_OK();
 }
 
@@ -1750,7 +1742,10 @@ bool WebRenderBridgeParent::ProcessWebRenderParentCommands(
         const OpAddCompositorAnimations& op =
             cmd.get_OpAddCompositorAnimations();
         CompositorAnimations data(std::move(op.data()));
-        if (!OwnsCompositorAnimationsId(data.id())) {
+        // AnimationHelper::GetNextCompositorAnimationsId() encodes the child
+        // process PID in the upper 32 bits of the id, verify that this is as
+        // expected.
+        if ((data.id() >> 32) != (uint64_t)OtherPid()) {
           gfxCriticalNote << "TOpAddCompositorAnimations bad id";
           success = false;
           continue;
@@ -2421,9 +2416,7 @@ mozilla::ipc::IPCResult WebRenderBridgeParent::RecvGetAnimationValue(
     return IPC_FAIL_NO_REASON(this);
   }
 
-  if (!OwnsCompositorAnimationsId(aCompositorAnimationsId)) {
-    *aValue = mozilla::null_t();
-  } else if (RefPtr<OMTASampler> sampler = GetOMTASampler()) {
+  if (RefPtr<OMTASampler> sampler = GetOMTASampler()) {
     Maybe<TimeStamp> testingTimeStamp;
     if (CompositorBridgeParent* cbp = GetRootCompositorBridgeParent()) {
       testingTimeStamp = cbp->GetTestingTimeStamp();
